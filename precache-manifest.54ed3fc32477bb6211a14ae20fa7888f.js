c2d4e0c2d972a0524e5a3a6f1869c4a5612ee071
Abhilash <krabhilash226@gmail.com>
2020-12-03 20:30:37 +0530

    Updates

---
+++ precache-manifest.54ed3fc32477bb6211a14ae20fa7888f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d5a87546f6f3c8b96ed018693fc81a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "336dbd43d029a4189379",
+    "url": "/react/static/css/main.298f8a35.chunk.css"
+  },
+  {
+    "revision": "0282d572f171f8d0e0f9",
+    "url": "/react/static/js/2.df7edbc7.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/react/static/js/2.df7edbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d11a6ca4955982876c3",
+    "url": "/react/static/js/3.e114a2e8.chunk.js"
+  },
+  {
+    "revision": "336dbd43d029a4189379",
+    "url": "/react/static/js/main.7878202e.chunk.js"
+  },
+  {
+    "revision": "1d33ca183ccd8c75cd7f",
+    "url": "/react/static/js/runtime-main.716d92be.js"
+  }
+]);
